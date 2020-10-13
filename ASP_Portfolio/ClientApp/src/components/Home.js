8b8d1dd6e5b76a48477bf63484e493fa9da8d283
Hongjo Lim <hongjolim@geotab.com>
2020-10-13 16:56:25 -0400

    Designed front_end

--- ASP_Portfolio/ClientApp/src/components/Home.js
+++ ASP_Portfolio/ClientApp/src/components/Home.js
@@ -6,16 +6,7 @@ export class Home extends Component {
 
   render () {
     return (
-          <section id="intro" className="wrapper style1 fullscreen fade-up">
-						<div className="inner">
-							<h1>Hongjo Lim</h1>
-							<p>Software Developer</p><br/>
-							<ul className="actions">
-								<li><a href="#one" className="button scrolly">Learn more</a></li>
-							</ul>
-						</div>
-					</section>
-        
+        <></>
     );
   }
 }
