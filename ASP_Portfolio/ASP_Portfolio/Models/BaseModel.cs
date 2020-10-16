89acde6d0f42aaea927d00db36e6d367c43e0a63
Hongjo Lim <czebahi@gmail.com>
2020-10-16 19:47:17 -0400

    Added entity framework packages and db context

---
+++ ASP_Portfolio/ASP_Portfolio/Models/BaseModel.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_Portfolio.Models
+{
+    public class BaseModel
+    {
+        [JsonProperty(Order = -2)]
+        public string Href { get; set; }
+    }
+}
---
+++ ASP_Portfolio/ASP_Portfolio/PortfolioDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ASP_Portfolio.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_Portfolio
+{
+    public class PortfolioDbContext : DbContext
+    {
+        public DbSet<Job> Jobs { get; set; }
+        
+        public DbSet<Project> Projects { get; set; }
+
+        public PortfolioDbContext(DbContextOptions options) : base(options) 
+        {
+
+        }
+    }
+}
